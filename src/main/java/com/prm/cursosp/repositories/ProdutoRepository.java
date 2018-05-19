c33b64d32724becd0127205ef596d0dcc516d297
PAULOMONTARROIOS <pauloricardomontarroios@hotmail.com>
2018-05-18 21:51:12 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/prm/cursosp/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.prm.cursosp.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.prm.cursosp.domain.Produto;
+
+@Repository
+public interface ProdutoRepository  extends JpaRepository<Produto, Integer>{
+
+}
