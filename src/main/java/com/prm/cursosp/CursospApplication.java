9ede472aba93c761b6fdb456d87dff8d63acec00
PAULOMONTARROIOS <pauloricardomontarroios@hotmail.com>
2018-05-18 21:28:21 -0300

    Criando operacao de instanciacao

--- src/main/java/com/prm/cursosp/CursospApplication.java
+++ src/main/java/com/prm/cursosp/CursospApplication.java
@@ -1,12 +1,31 @@
 package com.prm.cursosp;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.prm.cursosp.domain.Categoria;
+import com.prm.cursosp.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursospApplication {
+public class CursospApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1, cat2));
+		
 	}
 }
