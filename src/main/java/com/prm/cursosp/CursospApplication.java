14e2027b86530279cbc71f5afb4e3a9c04cec760
PAULOMONTARROIOS <pauloricardomontarroios@hotmail.com>
2018-05-19 22:31:03 -0300

    Estado e Cidade

--- src/main/java/com/prm/cursosp/CursospApplication.java
+++ src/main/java/com/prm/cursosp/CursospApplication.java
@@ -8,18 +8,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.prm.cursosp.domain.Categoria;
+import com.prm.cursosp.domain.Cidade;
+import com.prm.cursosp.domain.Estado;
 import com.prm.cursosp.domain.Produto;
 import com.prm.cursosp.repositories.CategoriaRepository;
+import com.prm.cursosp.repositories.CidadeRepository;
+import com.prm.cursosp.repositories.EstadoRepository;
 import com.prm.cursosp.repositories.ProdutoRepository;
 
 @SpringBootApplication
-public class CursospApplication implements CommandLineRunner{
-	
+public class CursospApplication implements CommandLineRunner {
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospApplication.class, args);
@@ -27,22 +37,34 @@ public class CursospApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... arg0) throws Exception {
-		Categoria cat1 = new Categoria(null,"Informática");
-		Categoria cat2 = new Categoria(null,"Escritório");
-		
-		Produto p1 = new Produto(null,"Computador",2000.00);
-		Produto p2 = new Produto(null,"Impressora",800.00);
-		Produto p3 = new Produto(null,"Mouse",80.00);
-		
-		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat2));
-		
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
 		categoriaRepository.save(Arrays.asList(cat1, cat2));
-		produtoRepository.save(Arrays.asList(p1,p2,p3));
-		
+		produtoRepository.save(Arrays.asList(p1, p2, p3));
+		estadoRepository.save(Arrays.asList(est1, est2));
+		cidadeRepository.save(Arrays.asList(c1, c2, c3));
+
 	}
 }
--- src/main/java/com/prm/cursosp/repositories/CidadeRepository.java
+++ src/main/java/com/prm/cursosp/repositories/CidadeRepository.java
@@ -3,9 +3,9 @@ package com.prm.cursosp.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.prm.cursosp.domain.Categoria;
+import com.prm.cursosp.domain.Cidade;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
 
 }
