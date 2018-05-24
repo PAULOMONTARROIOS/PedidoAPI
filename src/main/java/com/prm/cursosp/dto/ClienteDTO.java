018d72041cb9cb4abe8cfd17527c52235d29e40f
PAULOMONTARROIOS <pauloricardomontarroios@hotmail.com>
2018-05-24 13:45:29 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/prm/cursosp/dto/ClienteDTO.java
+++ src/main/java/com/prm/cursosp/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.prm.cursosp.domain.Cliente;
+import com.prm.cursosp.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/com/prm/cursosp/services/validation/ClienteUpdateValidator.java
+++ src/main/java/com/prm/cursosp/services/validation/ClienteUpdateValidator.java
@@ -2,53 +2,55 @@ package com.prm.cursosp.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.prm.cursosp.domain.Cliente;
-import com.prm.cursosp.domain.enums.TipoCliente;
-import com.prm.cursosp.dto.ClienteNewDTO;
+import com.prm.cursosp.dto.ClienteDTO;
 import com.prm.cursosp.repositories.ClienteRepository;
 import com.prm.cursosp.resources.exception.FieldMessage;
-import com.prm.cursosp.services.validation.utils.BR;
 
-public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+import javassist.compiler.Parser;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
 	
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
 	@Override
-	public void initialize(ClienteInsert ann) {
+	public void initialize(ClienteUpdate ann) {
 	}
 
 	@Override
-	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
 
-		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCodigo()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj", "CPF Inválido"));
-		}
+		@SuppressWarnings("unchecked")
+		Map<String,String> map = (Map<String,String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
 		
-		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
-		}
-
 		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
-		if(aux != null ) {
+		
+		if(aux != null && !aux.getId().equals(uriId)) {
 			list.add(new FieldMessage("email", "E-mail já existente"));
 		}
 		
+
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
 					.addConstraintViolation();
 		}
-		
-		
-		
+
 		return list.isEmpty();
 	}
 }
